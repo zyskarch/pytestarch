eaca2b101585cb2a9696e96e3a5091351c6f78f7
Benjamin Winkelmann <benjamin.winkelmann@tngtech.com>
2022-10-22 10:39:47 +0200

    Implement interfaces.

---
+++ src/pytestarch/diagram_import/diagram_parser.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+
+from pytestarch.diagram_import.parsed_dependencies import ParsedDependencies
+
+
+class DiagramParser(ABC):
+
+    @abstractmethod
+    def parse(self, file_path: Path) -> ParsedDependencies:
+        pass
+
+
+class PumlParser(DiagramParser):
+
+    def parse(self, file_path: Path) -> ParsedDependencies:
+        pass
---
+++ src/pytestarch/diagram_import/parsed_dependencies.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class ParsedDependencies:
+    all_modules: set[str]
+    dependencies: dict[str, set[str]]
+
