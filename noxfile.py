f9d0c3e0b5d83bbe0516fe91bc9b6e50cbe054df
finswimmer <finswimmer77@gmail.com>
2022-12-17 18:05:23 +0100

    chore: remove linter from noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -3,34 +3,6 @@ import nox
 locations = "src", "tests", "noxfile.py"
 
 
-@nox.session(python=["3.8"])
-def lint(session):
-    session.run("poetry", "shell")
-    session.run("poetry", "install", external=True)
-    session.run("poetry", "run", "flake8", *locations)
-
-
-@nox.session(python="3.8")
-def isort(session):
-    session.run("poetry", "shell")
-    session.run("poetry", "install", external=True)
-    session.run("poetry", "run", "isort", ".")
-
-
-@nox.session(python="3.8")
-def black(session):
-    session.run("poetry", "shell")
-    session.run("poetry", "install", external=True)
-    session.run("poetry", "run", "black", *locations)
-
-
-@nox.session(python="3.8")
-def bandit(session):
-    session.run("poetry", "shell")
-    session.run("poetry", "install", external=True)
-    session.run("poetry", "run", "bandit", "-r", "src")
-
-
 @nox.session(python=["3.8", "3.9", "3.10"])
 def tests(session):
     session.run("poetry", "shell")
