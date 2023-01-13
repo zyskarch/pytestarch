06aa8f7ee6d8537b4cfe5b812fbd949e65506f89
zyskarch <zyskarch@gmail.com>
2023-01-13 15:31:18 +0100

    Environment: Add support for python3.11.

--- noxfile.py
+++ noxfile.py
@@ -3,7 +3,7 @@ import nox
 locations = "src", "tests", "noxfile.py"
 
 
-@nox.session(python=["3.8", "3.9", "3.10"])
+@nox.session(python=["3.8", "3.9", "3.10", "3.11"])
 def tests(session):
     session.run("poetry", "shell")
     session.run("poetry", "install", external=True)
