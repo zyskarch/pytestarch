487e69cadfb53e8a9694795b2d438fad44402ff2
zyskarch <zyskarch@gmail.com>
2024-02-23 12:27:38 +0100

    Environment: Add support for python 3.12.

--- noxfile.py
+++ noxfile.py
@@ -5,7 +5,7 @@ import nox
 locations = "src", "tests", "noxfile.py"
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11"])
+@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
 def tests(session):
     session.run("poetry", "shell")
     session.run("poetry", "install", external=True)
