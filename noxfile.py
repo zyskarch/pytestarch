7d39767fbf73f8a2f94a9a5b99373086bed9c962
Raphael Boidol <boidolr@users.noreply.github.com>
2024-10-25 08:56:59 +0200

    Test for compatibility with Python 3.13

--- noxfile.py
+++ noxfile.py
@@ -5,7 +5,7 @@ import nox
 locations = "src", "tests", "noxfile.py"
 
 
-@nox.session(python=["3.9", "3.10", "3.11", "3.12"])
+@nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13"])
 def tests(session):
     session.run("poetry", "shell")
     session.run("poetry", "install", "--extras", "visualization", external=True)
