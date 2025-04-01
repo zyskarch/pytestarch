a467f5fb281d822bf0b8924320aa14070227630e
= <=>
2025-04-01 18:36:28 +0200

    Environment: Update to work with new poetry version.

--- noxfile.py
+++ noxfile.py
@@ -7,6 +7,6 @@ locations = "src", "tests", "noxfile.py"
 
 @nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13"])
 def tests(session):
-    session.run("poetry", "shell")
+    session.run("poetry", "env", "activate")
     session.run("poetry", "install", "--extras", "visualization", external=True)
     session.run("poetry", "run", "pytest")
