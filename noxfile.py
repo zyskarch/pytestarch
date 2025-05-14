e4e9527ca8fa0b26ce59e6a48d97c4d77931611a
Raphael Boidol <boidolr@users.noreply.github.com>
2025-05-14 22:07:41 +0200

    Infra: Update Python to supported versions
    - dropping 3.9 due to the version no longer having security support
    - adding 3.14 as new actively supported version

--- noxfile.py
+++ noxfile.py
@@ -5,7 +5,7 @@ import nox
 locations = "src", "tests", "noxfile.py"
 
 
-@nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13"])
+@nox.session(python=["3.10", "3.11", "3.12", "3.13", "3.14"])
 def tests(session):
     session.run("poetry", "env", "activate")
     session.run("poetry", "install", "--extras", "visualization", external=True)
