4ac36249bb5aa7aec02c4d1f21d05e26d51d736b
Raphael Boidol <boidolr@users.noreply.github.com>
2025-05-14 22:03:06 +0200

    Refactor: update noqa for tests to support `ruff` update

--- tests/resources/flat_test_project_2/runtime/runtime_importer.py
+++ tests/resources/flat_test_project_2/runtime/runtime_importer.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from flat_test_project_2.logging_util.logging_util_importee import l  # noqa: F401
-from flat_test_project_2.orchestration.orchestration_importee import (  # noqa: F401; noqa: F401 E401
+from flat_test_project_2.orchestration.orchestration_importee import (  # noqa: F401
     o,
 )
 from flat_test_project_2.persistence.persistence_importee import p  # noqa: F401
